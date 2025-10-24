3369043c22037084088eac913ead9915efb80e60
Pranav <pranavsresh1947@gmail.com>
2025-10-25 01:03:21 +0530

    Fixed backend transporter error

--- backend/routes/otpRoute.js
+++ backend/routes/otpRoute.js
@@ -15,6 +15,8 @@ const generateOtp = () => Math.floor(100000 + Math.random() * 900000).toString()
 // Configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: false,
+  port: 587,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
